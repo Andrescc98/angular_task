e06e673946a89216609ceb8f8c560ec80976331c
Andrescc98 <andresjosecc@gmail.com>
2020-09-26 05:30:04 -0400

    adicionando modulo de form y agr http_inter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { TokenInterceptor } from './interceptors/token.interceptor';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, RegisterComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
 
   providers: [
     {
