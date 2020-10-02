47e01d8066287cbebbd8e3533b9395ade6ba7c38
Andrescc98 <andresjosecc@gmail.com>
2020-10-02 03:21:00 -0400

    borrar sessionstorage si no hay sesion activa

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -24,17 +24,21 @@ export class AuthGuard implements CanActivate {
   canActivate(): Observable<boolean> | boolean {
 
     return this.userService.getUser().pipe(
+      // SI LA SESION ESTA ACTIVA
       map((res)=>{
         this.userService.setItem(res);
         this.loadService.its_ready_load();
         return true;
       }),
-
+      // DE NO ESTAR ACTIVA
       catchError((err)=>{
+        // SI HAY UN ERROR DISTINTO AL 401, ME LO MUESTRA POR CONSOLA 
         if(err.status !== 401){
           console.log(err);
         }
         this.loadService.its_ready_load();
+        this.userService.clear();
+        // REDIRECCIONA A LA RUTA "LOGIN"
         return of(this.route.createUrlTree(["/login"]));
       })
     )
