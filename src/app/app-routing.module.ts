0d21f0fd714a4bfa07257996b2bba63a5f8c2ac6
Andrescc98 <andresjosecc@gmail.com>
2020-10-02 01:47:22 -0400

    agregando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+/* COMPONENTS */
+import { IndexComponent } from './components/index/index.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  {path:"", canActivate:[AuthGuard], component:IndexComponent},
+  {path:"login", component:LoginComponent},
+  {path:"register", component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
