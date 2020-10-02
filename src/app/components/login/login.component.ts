920455ae37120b9bf3bd57ef591a6157d66b83ca
Andrescc98 <andresjosecc@gmail.com>
2020-10-02 02:05:21 -0400

    add metodo login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+import { LoadService } from 'src/app/services/load.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loading:boolean=false;
+
+  data:any={
+    email:"",
+    password:"",
+  };
+
+  constructor(
+    private tokenService:TokenService,
+    private userService:UserService,
+    private loadService:LoadService,
+    private route:Router
+  ) { }
 
   ngOnInit(): void {
   }
+
+  login(event:any){
+    event.preventDefault();
+    this.loading=true;
+
+    this.tokenService.getToken().subscribe(()=>{
+      this.userService.login(this.data).subscribe(
+        (res:any)=>{
+          this.loading=false;
+          this.route.navigate(["/"])
+        },
+        (err:any)=>{          
+          this.loading=false;
+          console.log(err.error.errors);
+        }
+      )
+    })
+  }
+
 
 }
