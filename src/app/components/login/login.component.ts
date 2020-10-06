2ac258b0b00b2a7916400416f042dac56de220a2
Andrescc98 <andresjosecc@gmail.com>
2020-10-05 22:26:53 -0400

    add formbuilder

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,48 +3,49 @@ import { TokenService } from 'src/app/services/token.service';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
 import { LoadService } from 'src/app/services/load.service';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  loading:boolean=false;
-
-  data:any={
-    email:"",
-    password:"",
-  };
+  loading: boolean = false;
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.minLength(4)]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
 
   constructor(
-    private tokenService:TokenService,
-    private userService:UserService,
-    private loadService:LoadService,
-    private route:Router
-  ) { }
+    private tokenService: TokenService,
+    private userService: UserService,
+    private route: Router,
+    private fb: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  login(event:any){
+  login(event: any) {
     event.preventDefault();
-    this.loading=true;
+    this.loading = true;
 
-    this.tokenService.getToken().subscribe(()=>{
-      this.userService.login(this.data).subscribe(
-        (res:any)=>{
-          this.loading=false;
-          this.route.navigate(["/"])
+    this.tokenService.getToken().subscribe(() => {
+      this.userService.login(this.form.value).subscribe(
+        (res: any) => {
+          this.loading = false;
+          this.route.navigate(['/']);
         },
-        (err:any)=>{          
-          this.loading=false;
+        (err: any) => {
+          this.loading = false;
           console.log(err.error.errors);
         }
-      )
-    })
+      );
+    });
   }
-
-
 }
